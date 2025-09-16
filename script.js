4b82dab377674ba62db9eb87ea2826a912b3358f
Mark <mark.mkrtichyan.mm@gmail.com>
2025-09-16 21:48:31 +0400

    16.09 js hmwrk

--- script.js
+++ script.js
@@ -1,11 +1,85 @@
-let prompt = require('prompt-sync')()
-let num = Number(prompt("Enter a number:"));
-if (num > 0) {
-    console.log(num + " is a positive number.");
-}
-else if (num == 0) {
-    console.log(num + " is zero.");
-}
-else {
-    console.log(num + " is a negative number.");
-}
+// #1.
+
+// let prompt = require('prompt-sync')()
+// let num = Number(prompt("Enter a number:"));
+// if (num > 0) {
+//     console.log(num + " is a positive number.");
+// }
+// else if (num == 0) {
+//     console.log(num + " is zero.");
+// }
+// else {
+//     console.log(num + " is a negative number.");
+// }
+
+
+// #2.
+
+// let result;
+
+// let a = 4;
+// let b = 2;
+
+// result = (a + b < 4) ? 'Less' : 'More';
+
+// console.log(result);
+
+
+// #3.
+
+// for (let i = 1; i <= 40; i+=2) {
+//     if (i / 10 >= 1)
+//         console.log(i);
+// }
+
+
+// #4.
+
+// let num = 8;
+
+// for (let i = 0; i <= 9; i++) {
+//     console.log(num + "*" + i + "=" + (num*i));
+// }
+
+
+// #5.
+
+// let sum = 0
+// for (let i = 1; i <= 14; i++) {
+//     sum += i; 
+// }
+// console.log(sum);
+
+
+// #6.
+
+// let sum = 0
+// for (let i = 1; i <= 40; i++) {
+//     if (i % 2 == 0)
+//         sum += i;     
+// }
+// console.log(sum);
+
+
+// #7.
+
+// let i = 1;
+// let sum = 0;
+// while(i < 40){
+//     if (i % 2 != 0){
+//         sum += i;
+//     }
+//     i++; 
+// }
+// console.log(sum);
+
+
+// #8.
+
+// let mult = 1;
+// for (let i = 1; i <= 60; i++) {
+//     if (i % 6 == 0) {
+//         mult *= i;
+//     }
+// }
+// console.log(mult);
