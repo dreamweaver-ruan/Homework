f88015e194e84a859fc6b650adf8c78e00eb859d
Mark <mark.mkrtichyan.mm@gmail.com>
2025-09-27 20:46:33 +0400

    added objects27.09.js

---
+++ objects27.09.js
@@ -0,0 +1,43 @@
+// 1.
+
+// let person = {
+//     name: 'Mark',
+//     age: 18,
+//     city: 'Yerevan'
+// };
+
+// person.isStudent = true;
+
+// delete person.city;
+
+// if ('age' in person){
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+// console.log(person);
+
+
+//2.
+
+// let obj = {
+// 	key1: 'hello',
+// 	key2: undefined,
+// 	key3: 123,
+// 	key4: undefined
+// }
+
+
+
+// function delUndef(){
+//     for (key in obj){
+//         if (obj[key] == undefined){
+//             delete obj[key];
+//         }
+//     }
+// }
+
+// delUndef()
+// console.log(obj)
